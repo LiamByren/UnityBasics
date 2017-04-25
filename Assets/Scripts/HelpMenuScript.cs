7c417235fc7a93fb846de4551736f866a2900bba
LiamByren <liambyren101@gmail.com>
2017-04-25 12:49:21 +0200

    GIT 12
    Finished help menu

---
+++ Assets/Scripts/HelpMenuScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HelpMenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("escape"))
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
